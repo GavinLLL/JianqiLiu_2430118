d3a9fbf6a0cbe72310bb26af72d8397eb8306b37
GavinLLL <gavin.era@gmail.com>
2016-09-21 10:57:54 -0700

    Signed-off-by: GavinLLL <gavin.era@gmail.com>

---
+++ Hmwk/Assignment_2/Gaddis_8thED_Chap3_Prog2/main.cpp
@@ -0,0 +1,30 @@
+/* 
+ * File:   main.cpp
+ * Author: Jianqi Liu
+ * Purpose:
+ * Created on September 21, 2016, 10:48 AM
+ */
+
+
+//System Libraries
+#include <iostream>  //Input/out objects
+using namespace std; //Name-space used in system Library 
+
+//User Libraries
+
+//Global constants
+
+//Function prototypes
+
+//Execution begins here!
+int main(int argc, char** argv) {
+    //Declaration of variables
+    
+   //Process values-> Map inputs to Outputs
+    
+    //Display output
+    
+    //Exit program
+    
+    return 0;/*
+ 
