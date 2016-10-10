0e152b7a58fee985c38a1b53bc09469d3d7c6f82
gavinLLL <gavin.era@gmail.com>
2016-10-10 09:57:41 -0700

    Signed-off-by: gavinLLL <gavin.era@gmail.com>

---
+++ Lab/MenuWithFunctions/main.cpp
@@ -0,0 +1,118 @@
+/* 
+ * File:   main.cpp
+ * Author: Jianqi Liu
+ * Created on October 10, 2016, 9:32 AM
+ * Purpose:
+ */
+
+
+//System Libraries
+#include <iostream>  //Input/out objects
+using namespace std; //Name-space used in system Library 
+
+//User Libraries
+
+//Global constants
+
+//Function prototypes
+
+//Execution begins here!
+int main(int argc, char** argv) {
+    //Declaration of variables
+    int menuItm;  //Choice from menu
+    
+    //Prompt for problem for user input
+    cout<<"1.Type 1 for Problem A"<<endl;
+    cout<<"2.Type 2 for Problem B"<<endl;
+    cout<<"3.Type 3 for Problem C"<<endl;
+    cout<<"4.Type 4 for Problem D"<<endl;
+    cout<<"5.Type 5 for Problem E"<<endl;
+    cout<<"6.Type 6 for Problem F"<<endl;
+    cout<<"7.Type 7 for Problem G"<<endl;
+    cout<<"8.Type 8 for Problem H"<<endl;
+    cout<<"9.Type 9 for Problem I"<<endl;
+    cout<<"10.Type 10 for Problem J"<<endl;
+    cin>>menuItm;
+    //Go to the Problem
+    switch(menuItm){
+        case1:{
+            cout<<"Inside Problem A"<<endl;
+            //Input values
+             //Process values-> Map inputs to Outputs
+            //Display output
+            break;
+        }
+        case2:{
+            cout<<"Inside Problem B"<<endl;
+            //Input values
+             //Process values-> Map inputs to Outputs
+            //Display output
+            break;
+        }
+        case3:{
+            cout<<"Inside Problem C"<<endl;
+            //Input values
+             //Process values-> Map inputs to Outputs
+            //Display output
+            break;
+        }
+        case4:{
+            cout<<"Inside Problem D"<<endl;
+            //Input values
+             //Process values-> Map inputs to Outputs
+            //Display output
+            break;
+        }
+        case5:{
+            cout<<"Inside Problem E"<<endl;
+            //Input values
+             //Process values-> Map inputs to Outputs
+            //Display output
+            break;
+        }
+        case6:{
+            cout<<"Inside Problem F"<<endl;
+            //Input values
+             //Process values-> Map inputs to Outputs
+            //Display output
+            break;
+        }
+        case7:{
+            cout<<"Inside Problem G"<<endl;
+            //Input values
+             //Process values-> Map inputs to Outputs
+            //Display output
+            break;
+        }
+        case8:{
+            cout<<"Inside Problem H"<<endl;
+            //Input values
+             //Process values-> Map inputs to Outputs
+            //Display output
+            break;
+        }
+        case9:{
+            cout<<"Inside Problem I"<<endl;
+            //Input values
+             //Process values-> Map inputs to Outputs
+            //Display output
+            break;
+        }
+        case10:{
+            cout<<"Inside Problem J"<<endl;
+            //Input values
+             //Process values-> Map inputs to Outputs
+            //Display output
+            break;
+        }//End case
+    }
+    //Input values
+    
+    //Process values-> Map inputs to Outputs
+    
+    //Display output
+    
+    //Exit program
+    
+    return 0;
+}
---
+++ Lab/MenuWithFunctions_Examples/main.cpp
@@ -0,0 +1,38 @@
+/* 
+ * File:   main.cpp
+ * Author: Jianqi Liu
+ * Created on September 27, 2016, 4:35 PM
+ * Purpose:
+ */
+
+
+//System Libraries
+#include <iostream>  //Input/out objects
+using namespace std; //Name-space used in system Library 
+
+//User Libraries
+
+//Global constants
+float GRAVITY=6.673e-8f;  //Universal Gravity Constant cm^3/g/sec^2
+float CNKGGRM=1000;       //Conversion from KG to Grams
+float CNSGGRM=14593.9;    //Conversion from Slugs to Grams
+float CNKMMET=1000;       //Conversion from Km to Meters
+float CNMETCM=100;        //Conversion from Meters to Centimeters
+float CNDYNLB=2.24809e-6f;//Conversion from Dynes to Pounds
+float CNSGLBS=32.174f;    //Conversion from slugs to lbs which oddly acceleration of gravity in ft/sec^2
+//Function prototypes
+
+//Execution begins here!
+int main(int argc, char** argv) {
+    //Declaration of variables
+    
+    //Input values
+    
+    //Process values-> Map inputs to Outputs
+    
+    //Display output
+    
+    //Exit program
+    
+    return 0;
+}
